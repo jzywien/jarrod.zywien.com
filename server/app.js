45a7f232f27bc03d3f54af32f45409ec209ab0ad
jzywien <jzywien@gmail.com>
2018-06-29 10:36:37 -0400

    Initial commit

---
+++ server/app.js
@@ -0,0 +1,22 @@
+const _ = require('lodash');
+const express = require('express');
+const favicon = require('serve-favicon')
+const path = require('path');
+const bodyParser = require('body-parser');
+const api = require('./api');
+const DISTDIR = path.join(__dirname, '../dist');
+
+const app = express();
+
+app.use(favicon(path.join( DISTDIR, 'favicon.ico')));
+app.use(express.static(DISTDIR));
+app.use('/', express.static(DISTDIR));
+
+app.use(bodyParser.json());
+app.use('/api', api);
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
